78114892bb3cd9c2647fd3d6519b54ab82ec9753
Mateusz Erwinski <101421783+AirVine64@users.noreply.github.com>
2023-01-25 16:54:50 +0100

    Add while loop

--- src/main.rs
+++ src/main.rs
@@ -2,52 +2,69 @@
 use std::io;
 use std::process;
 
-fn main() {
-    //Values input
-    println!("Input the first value");
-    let mut value1 = String::new();
-    io::stdin().read_line(&mut value1).expect("Failed to read line");
-
-    println!("Input the second value");
-    let mut value2 = String::new();
-    io::stdin().read_line(&mut value2).expect("Failed to read line");
-
-    //Type casting
-    let number1: i128 = value1.trim().parse().unwrap();
-    let number2: i128 = value2.trim().parse().unwrap();
-
-    //Operation input
-    println!("Input the operation of your choice (+/-/*/:/^)");
-    let mut operation = String::new();
-    io::stdin().read_line(&mut operation).expect("Failed to read line");
-
-    //Result calculation
-    let outcome: i128;
-    if operation.trim() == "+"{
-        outcome = number1 + number2;
-    }
-    else if operation.trim() == "-" {
-        outcome = number1 - number2;
-    }
-    else if operation.trim() == "*" {
-        outcome = number1 * number2;
-    }
-    else if operation.trim() == "/" || operation.trim() == ":" {
-        outcome = number1 / number2;
-    }
-    else if operation.trim() == "^"{
-        outcome = number1.pow(number2 as u32);
-    }
-    else{
-        println!("Invalid operation");
+fn exit_test(var: String) {
+    if var.trim() == "exit" || var.trim() == "Exit" {
         process::exit(0);
     }
+}
 
-    //Result printout
-    println!("{}", outcome);
+fn main() {
+    //Variables declarations
+    let mut value1 = String::new();
+    let mut value2 = String::new();
+    let mut operation = String::new();
+    let mut number1: i128;
+    let mut number2: i128;
+    let mut outcome: i128;
 
-    //Exit
-    println!("Press enter to exit");
-    let mut exit = String::new();
-    io::stdin().read_line(&mut exit).expect("Failed to read line");
+    //Main program loop
+    loop {
+        //Values input
+        println!("Input the first value");
+        io::stdin().read_line(&mut value1).expect("Failed to read line");
+        //exit_test(value2);
+
+        println!("Input the second value");
+        io::stdin().read_line(&mut value2).expect("Failed to read line");
+        //exit_test(value2);
+
+        //Type casting
+        number1 = value1.trim().parse().unwrap();
+        number2 = value2.trim().parse().unwrap();
+
+        //Operation input
+        println!("Input the operation of your choice (+/-/*/:/^)");
+        io::stdin().read_line(&mut operation).expect("Failed to read line");
+        //exit_test(operation);
+
+        //Result calculation
+        loop {
+            if operation.trim() == "+"{
+                outcome = number1 + number2;
+                break;
+            }
+            else if operation.trim() == "-" {
+                outcome = number1 - number2;
+                break;
+            }
+            else if operation.trim() == "*" {
+                outcome = number1 * number2;
+                break;
+            }
+            else if operation.trim() == "/" || operation.trim() == ":" {
+                outcome = number1 / number2;
+                break;
+            }
+            else if operation.trim() == "^"{
+                outcome = number1.pow(number2 as u32);
+                break;
+            }
+            else{
+                println!("Invalid operation");
+            } 
+        }
+
+        //Result printout
+        println!("{}", outcome);
+    }
 }
