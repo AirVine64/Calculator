d95482b8f0cdfba801abefab40ece28d48f11ff0
Mateusz Erwinski <101421783+AirVine64@users.noreply.github.com>
2023-01-25 17:04:32 +0100

    Add exit feature

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,6 @@
 use std::io;
 use std::process;
 
-fn exit_test(var: String) {
-    if var.trim() == "exit" || var.trim() == "Exit" {
-        process::exit(0);
-    }
-}
-
 fn main() {
     //Variables declarations
     let mut value1 = String::new();
@@ -19,23 +13,31 @@ fn main() {
 
     //Main program loop
     loop {
-        //Values input
+        //Values input and type casting
         println!("Input the first value");
         io::stdin().read_line(&mut value1).expect("Failed to read line");
-        //exit_test(value2);
+        if value1.trim() == "exit" {
+            process::exit(0);
+        }
+        else {
+            number1 = value1.trim().parse().unwrap();
+        }
 
         println!("Input the second value");
         io::stdin().read_line(&mut value2).expect("Failed to read line");
-        //exit_test(value2);
-
-        //Type casting
-        number1 = value1.trim().parse().unwrap();
-        number2 = value2.trim().parse().unwrap();
+        if value2.trim() == "exit" {
+            process::exit(0);
+        }
+        else {
+            number2 = value2.trim().parse().unwrap();
+        }
 
         //Operation input
         println!("Input the operation of your choice (+/-/*/:/^)");
         io::stdin().read_line(&mut operation).expect("Failed to read line");
-        //exit_test(operation);
+        if operation.trim() == "exit" {
+            process::exit(0);
+        }
 
         //Result calculation
         loop {
